c43e7abbd330c30caf9213b11036ab690a922f6a
leonidasrsantos <126978612+leonidasrsantos@users.noreply.github.com>
2023-04-27 00:33:17 -0300

    sistema de validação
    Reconhecer quem ganhou

--- testes/jogodavelha2/script.js
+++ testes/jogodavelha2/script.js
@@ -1,4 +1,4 @@
-let jogador = 'X'
+let jogador = 'o'
 let space = window.document.querySelectorAll('div#jogo td')
 
 for (let pos in space){
@@ -13,4 +13,20 @@ for (let pos in space){
 }
 
 function ganhou(){
+    let a = 0;let b = 1;let c = 2;  let d = 0;let f = 3;let g = 6;  let h = 0;let i = 4;let j = 8;
+    //Validação vertical e horizontal
+        for (c; c <= 9; c+=3){
+            if (space[a].textContent && space[b].textContent && space[c].textContent ||space[d].textContent && space[f].textContent && space[g].textContent  == jogador){
+                console.log('horizontal e vertical')
+            }
+            a+=3;b+=3;  d++;f++;g++;
+        }
+    //Validação diagonal
+        for (h; h < 4; h+=2){ 
+            if(space[h].textContent && space[i].textContent && space[j].textContent == jogador){
+                console.log('diagonal')
+            }
+            j-=2; 
+        }
+        
 }
